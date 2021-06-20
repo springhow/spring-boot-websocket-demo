b9f757f5d29d94845160696ad6079d0325751e4b
Raja Anbazhagan <raja.anbazhagan1991@gmail.com>
2021-06-20 22:08:44 +0530

    Initial commit

---
+++ src/main/java/com/example/springbootwebsocket/ChatMessage.java
@@ -0,0 +1,22 @@
+package com.example.springbootwebsocket;
+
+public class ChatMessage {
+    private String name;
+    private String message;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/example/springbootwebsocket/ChatMessageHandler.java
@@ -0,0 +1,37 @@
+package com.example.springbootwebsocket;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ChatMessageHandler extends TextWebSocketHandler {
+
+    List<WebSocketSession> webSocketSessions = Collections.synchronizedList(new ArrayList<>());
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+        webSocketSessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+        webSocketSessions.remove(session);
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+
+        super.handleMessage(session, message);
+        for (WebSocketSession webSocketSession : webSocketSessions) {
+            webSocketSession.sendMessage(message);
+        }
+    }
+
+}
